ac47193e48b7d18ac334f23f434c864f64620aaf
Luthfi Fadillah <luthfi.fadillah@outlook.com>
2017-03-02 09:57:19 +0700

    Updated BigHornSheep.h

--- BigHornSheep.h
+++ BigHornSheep.h
@@ -2,21 +2,14 @@
 #define BIGHORNSHEEP_H
 #include "Artiodactyls.h"
 #include "Animals.h"
+
 class BigHornSheep:public Artiodactyls{
 //method
 public:
 	//ctor with param
-	BigHornSheep(int bb, grid koordinat);
-	//copy costructor
-	BigHornSheep (const BigHornSheep&);
-	//destructor
-	~BigHornSheep();
+	BigHornSheep(int bb);
+
 	//polymorphism interaksi
 	void interact();
-
-
-//atribut
-private:
-
 };
 #endif
